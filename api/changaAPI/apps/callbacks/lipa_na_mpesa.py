9ec2d66a3a306f1acb14afd6b793e5af460b7c4a
TheSteelGuy <collotuos0@gmail.com>
2019-08-11 00:09:32 +0300

    allow saving if resulcode is 0 ie succeful

--- api/changaAPI/apps/callbacks/lipa_na_mpesa.py
+++ api/changaAPI/apps/callbacks/lipa_na_mpesa.py
@@ -15,6 +15,7 @@ class LipaNaMpesaView(generics.CreateAPIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, **kwargs):
+
         print('....................................................................≥≥...................')
         phone = request.data.get('MSISDN')
         phone_number = phone if len(phone) <= 10 else phone[3:]
@@ -26,53 +27,44 @@ class LipaNaMpesaView(generics.CreateAPIView):
                 title='chamaa'+request.data.get('BusinessShortCode'),
                 account_number=request.data.get('BusinessShortCode'),
             )
-  
+        data = request.data.get('Body')
+        if data['stkCallback']['ResultCode'] == 0:
+            contribution_obj = Contribution(
+                transaction_type=request.data.get('TransactionType'),
 
-        contribution_obj = Contribution(
-            transaction_type=request.data.get('TransactionType'),
+                transaction_id=request.data.get('TransID'),
 
-            transaction_id=request.data.get('TransID'),
+                amount=request.data.get('TransAmount'),
 
-            amount=request.data.get('TransAmount'),
+                business_shortcode=request.data.get('BusinessShortCode'),
 
-            business_shortcode=request.data.get('BusinessShortCode'),
+                bill_ref_number=request.data.get('BillRefNumber'),
 
-            bill_ref_number=request.data.get('BillRefNumber'),
+                account_balance=request.data.get('OrgAccountBalance'),
 
-            account_balance=request.data.get('OrgAccountBalance'),
+                msisdn=request.data.get('MSISDN'),
 
-            msisdn=request.data.get('MSISDN'),
-
-            first_name=request.data.get('FirstName'),
-            middle_name=request.data.get('MiddleName'),
-            last_name=request.data.get('LastName'),
-            required_amount = chamaa_obj.required_amount
-        )
-
-        #contribution_obj.save()
-
-        user = User.objects.filter(phone_number__contains=phone_number).first()
-
-        if not user:
-            user = User.objects.create_user(
-                phone_number=phone_number,
-                username='user'+phone_number,
-                password=phone_number
+                first_name=request.data.get('FirstName'),
+                middle_name=request.data.get('MiddleName'),
+                last_name=request.data.get('LastName'),
+                required_amount=chamaa_obj.required_amount
             )
 
-        # calculations function
-        add_context_to_contribution(contribution_obj, chamaa_obj, user, Contribution)
+            user = User.objects.filter(phone_number__contains=phone_number).first()
 
-        # user.contributions.add(contribution_obj)
+            if not user:
+                user = User.objects.create_user(
+                    phone_number=phone_number,
+                    username='user'+phone_number,
+                    password=phone_number
+                )
 
+            # calculations function
+            add_context_to_contribution(contribution_obj, chamaa_obj, user, Contribution)
 
-        # chamaa_obj.contributions.add(contribution_obj)
-        # user.chamaas.add(chamaa_obj)
-
-
-        # print(contribution_obj)
-
-        return Response({'message': request.data}, status=status.HTTP_200_OK)
+            return Response({'message': 'Thank you dear member, your contribution has been recorded'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Something went wrong your transaction was not completed'},status=status.HTTP_400_BAD_REQUEST)
 
 
 class MpesaValidationUrl(generics.CreateAPIView):
@@ -84,4 +76,3 @@ class MpesaValidationUrl(generics.CreateAPIView):
         print(request.data)
 
         return Response(status=status.HTTP_200_OK)
-# {'Body': {'stkCallback': {'MerchantRequestID': '18430-5217758-1', 'CheckoutRequestID': 'ws_CO_DMZ_389317553_19072019104628016', 'ResultCode': 0, 'ResultDesc': 'The service request is processed successfully.', 'CallbackMetadata': {'Item': [{'Name': 'Amount', 'Value': 1.0}, {'Name': 'MpesaReceiptNumber', 'Value': 'NGJ6D6ZLDS'}, {'Name': 'Balance'}, {'Name': 'TransactionDate', 'Value': 20190719104640}, {'Name': 'PhoneNumber', 'Value': 254723135671}]}}}}
